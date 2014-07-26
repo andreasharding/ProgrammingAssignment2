63cc3f759b33f78bcf211d018b8f0bc193f811bf
andreasharding <andreas.a.harding@gmail.com>
2014-07-26 18:07:59 +0100

    commented solution with test cases

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+#' Memoization: Assignment to cache computationally expensive results
 
-## factory for inverse object, based on mean example
+
+#' factory for inverse object, based on mean example
+#' @param x - a matrix whose inverse is to be calculated
+#' @return list object with set/get for both x and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -19,7 +21,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## returns cached version or calculates value if not cached, then caches it
+#' returns cached version or calculates value if not cached, then caches it
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
@@ -34,3 +36,31 @@ cacheSolve <- function(x, ...) {
     inv
     
 }
+
+## test cases
+
+## make some simple matrices for testing
+a <- matrix(c(2,4,6,8), nrow = 2, ncol = 2, byrow = TRUE)
+b <- matrix(c(2,3,5,7), nrow = 2, ncol = 2, byrow = FALSE)
+c <- matrix(c(1,1,2,4), nrow = 2, ncol = 2, byrow = FALSE)
+
+## solve these for reference of good output
+solve(a)
+solve(b)
+solve(c)
+
+## now make the function objects/lists
+aa <- makeCacheMatrix(a)
+bb <- makeCacheMatrix(b)
+cc <- makeCacheMatrix(c)
+
+## then perform memoized solve...
+## first time has to calculate inverse
+cacheSolve(aa)
+cacheSolve(bb)
+cacheSolve(cc)
+
+## second time around can retrieve values from cache (and prints message)
+cacheSolve(aa)
+cacheSolve(bb)
+cacheSolve(cc)
